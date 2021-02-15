2124c285c3f78d46cf13b718fdc46ec91739beba
vinicius-oliveira7787 <vinicius.oliveira7787@gmail.com>
2021-02-15 15:43:02 -0300

    correção styled

--- src/components/Funcionalidades/Funcionalidades.jsx
+++ src/components/Funcionalidades/Funcionalidades.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Badge from 'react-simple-badges'
 import { Jumbotron, Table, Container } from "react-bootstrap"
-import { Tittle, P, Ul } from './styles'
+import { Tittle, Ul } from './styles'
 
 var functionalitiesArray = [
-  'Filtração de melhores preços por produto',
-  'Notificação via e-mail ou WhatsApp quando o produto desejável se encontra no preço esperado',
-  'Recomendações de produtos com base nas recentes pesquisas do usuário',
-  'Exibição da lista de produtos favoritados pelo usuário',
-  'Histórico de preços do produto'
+  'Filtração de melhores preços por produto;',
+  'Notificação via e-mail ou WhatsApp quando o produto desejável se encontra no preço esperado;',
+  'Recomendações de produtos com base nas recentes pesquisas do usuário;',
+  'Exibição da lista de produtos favoritados pelo usuário;',
+  'Histórico de preços do produto.'
 ]
 
 
--- src/components/Funcionalidades/styles.js
+++ src/components/Funcionalidades/styles.js
@@ -4,15 +4,8 @@ export const Tittle = styled.h2`
   padding: 5px;
 `
 
-export const P = styled.p`
-  padding-top: 3x;
-  padding-bottom: 3x;
-  font-size: 20px;
-`
-
 export const Ul = styled.ul`
   padding-top: 3x;
   padding-bottom: 3x;
   font-size: 20px;
-  list-style: none;
 `
