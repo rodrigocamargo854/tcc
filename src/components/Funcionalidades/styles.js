98527c246b6dacbec71f114fd3cabb0a9454bb94
vinicius-oliveira7787 <vinicius.oliveira7787@gmail.com>
2021-02-15 15:53:23 -0300

    final commit

--- src/components/Funcionalidades/styles.js
+++ src/components/Funcionalidades/styles.js
@@ -1,14 +1,7 @@
 import styled from 'styled-components'
 
 export const Tittle = styled.h2`
-  padding: 5px;
-`
-
-export const Ul = styled.ul`
-  padding-top: 3x;
-  padding-bottom: 3x;
-  font-size: 20px;
-  display: block;
+  padding-bottom: 5px;
 `
 
 export const P = styled.p`
