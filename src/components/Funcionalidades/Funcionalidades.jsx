84b69d858e93e365fe7268e1ad38c48397293257
vinicius-oliveira7787 <vinicius.oliveira7787@gmail.com>
2021-02-15 15:39:09 -0300

    funcionalidades

--- src/components/Funcionalidades/Funcionalidades.jsx
+++ src/components/Funcionalidades/Funcionalidades.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import Badge from 'react-simple-badges'
 import { Jumbotron, Table, Container } from "react-bootstrap"
+import { Tittle, P, Ul } from './styles'
 
-
+var functionalitiesArray = [
+  'Filtração de melhores preços por produto',
+  'Notificação via e-mail ou WhatsApp quando o produto desejável se encontra no preço esperado',
+  'Recomendações de produtos com base nas recentes pesquisas do usuário',
+  'Exibição da lista de produtos favoritados pelo usuário',
+  'Histórico de preços do produto'
+]
 
 
 function Funcionalidades() {
-
   return (
     <>
-      
       <Jumbotron style={{ backgroundColor: "#282c34" }}>
-        <h1><b>FUNCIONALIDADES</b></h1>
-        <p>acesse</p>
+        <Tittle>FUNCIONALIDADES</Tittle>
+        <Ul>{functionalitiesArray}</Ul>
         <img src="./logoicone.png" alt=""/>
       </Jumbotron>
-
-
     </>
   );
 }
---
+++ src/components/Funcionalidades/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const Tittle = styled.h2`
+  padding: 5px;
+`
+
+export const P = styled.p`
+  padding-top: 3x;
+  padding-bottom: 3x;
+  font-size: 20px;
+`
+
+export const Ul = styled.ul`
+  padding-top: 3x;
+  padding-bottom: 3x;
+  font-size: 20px;
+  list-style: none;
+`
