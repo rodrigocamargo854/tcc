7cafaddf76ab3a8818b0ef087be83b7b84785bb7
vinicius-oliveira7787 <vinicius.oliveira7787@gmail.com>
2021-02-15 15:56:47 -0300

    font-size

--- src/components/Funcionalidades/styles.js
+++ src/components/Funcionalidades/styles.js
@@ -1,12 +1,11 @@
 import styled from 'styled-components'
 
-export const Tittle = styled.h2`
+export const Tittle = styled.h1`
   padding-bottom: 5px;
 `
 
-export const P = styled.p`
+export const P = styled.h2`
   padding-top: 3x;
   padding-bottom: 3x;
-  font-size: 20px;
   display: block;
 `
