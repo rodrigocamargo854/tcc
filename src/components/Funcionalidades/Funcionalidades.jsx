25527e2afdfa54cae1080118bd54ca10e4c92557
vinicius-oliveira7787 <vinicius.oliveira7787@gmail.com>
2021-02-21 10:16:06 -0300

    funcionalidades

--- src/components/Funcionalidades/Funcionalidades.jsx
+++ src/components/Funcionalidades/Funcionalidades.jsx
@@ -3,25 +3,16 @@ import Badge from 'react-simple-badges'
 import { Jumbotron, Table, Container } from "react-bootstrap"
 import { Tittle, P} from './styles'
 
-var functionalitiesArray = [
-  'Filtração de melhores preços por produto;',
-  'Notificação via e-mail ou WhatsApp quando o produto desejável se encontra no preço esperado;',
-  'Recomendações de produtos com base nas recentes pesquisas do usuário;',
-  'Exibição da lista de produtos favoritados pelo usuário;',
-  'Histórico de preços do produto.'
-]
-
-
 function Funcionalidades() {
   return (
     <>
       <Jumbotron style={{ backgroundColor: "#282c34" }}>
         <Tittle>FUNCIONALIDADES</Tittle>
         <br/>
-        <P>Notificação via e-mail ou WhatsApp quando o produto desejável se encontra no preço esperado;</P>
+        <P>Notificações via e-mail ou sms quando o produto desejável se encontrar no preço esperado;</P>
         <P>Recomendações de produtos com base nas recentes pesquisas do usuário;</P>
         <P>Exibição da lista de produtos favoritados pelo usuário;</P>
-        <P>Filtração de melhores preços por produto;</P>
+        <P>Filtro dos melhores preços por produto;</P>
         <P>Histórico de preços do produto.</P>
         <br/>
       </Jumbotron>
