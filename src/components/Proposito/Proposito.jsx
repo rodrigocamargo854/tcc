ad23ed5dc4e7640614be79f503030540bcc917d4
rodrigocamargo <rodrigocamargo854@gmail.com>
2021-02-22 11:15:39 -0300

    update

--- src/components/Proposito/Proposito.jsx
+++ src/components/Proposito/Proposito.jsx
@@ -15,7 +15,7 @@ function Proposito() {
         <br></br>
         <h2>
           Oferecer versatilidade para buscas em ofertas e produtos priorizando
-          vendededores e lojas com alta classificação na plataforma MercadoLivre por meio de WebCrawler.
+          vendedores e lojas com alta classificação na plataforma MercadoLivre por meio de WebCrawler.
         </h2>  
       </Jumbotron>
     </>
