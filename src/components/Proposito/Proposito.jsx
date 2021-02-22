267ec6871eaa843567343643bf6aec7ff6bcb9bd
ArtuoS <arthureduardomolinari@gmail.com>
2021-02-22 19:10:23 -0300

    proposito

--- src/components/Proposito/Proposito.jsx
+++ src/components/Proposito/Proposito.jsx
@@ -15,7 +15,7 @@ function Proposito() {
         <br></br>
         <h2>
           Oferecer versatilidade para buscas em ofertas e produtos priorizando
-          vendedores e lojas com alta classificação na plataforma MercadoLivre por meio de WebCrawler.
+          vendedores e lojas com alta classificação na plataforma MercadoLivre por meio do consumo da sua API.
         </h2>  
       </Jumbotron>
     </>
