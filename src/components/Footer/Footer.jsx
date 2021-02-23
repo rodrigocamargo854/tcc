d41055751ba27c472f17ef973211098ab79f9c3b
rodrigocamargo <rodrigocamargo854@gmail.com>
2021-02-23 18:37:50 -0300

    update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -20,7 +20,7 @@ const Footer = () => {
           <a href="https://www.linkedin.com/in/marcos-vinicius-rocha-davi-alves-4b133072">
             <img style={{padding:10}} align="left" alt="Rodrigo Camargo Linkedin" color="white" width="52pxpx" src="https://raw.githubusercontent.com/peterthehan/peterthehan/master/assets/linkedin.svg" />
           </a>  
-          <a href="https://www.linkedin.com/in/vinicius-oliveira-2273821a0">
+          <a href="https://www.linkedin.com/in/vinicius-de-oliveira-2273821a0/">
             <img style={{padding:10}} align="left" alt="Rodrigo Camargo Linkedin" color="white" width="52pxpx" src="https://raw.githubusercontent.com/peterthehan/peterthehan/master/assets/linkedin.svg" />
           </a>      
            
