efe1f2d9ca716dc23b7d8c6cf717d297d771851e
MatheusTallmann <matheusstma@gmail.com>
2021-02-24 00:38:30 -0300

    nomes linkedin

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,20 +9,24 @@ const Footer = () => {
       <FooterStyled>
         <Table style={{color:"white"}}>
         <address>
-          <div style={{marginLeft:0}}>
+          <div style={{marginLeft:0, }}>
           
-            <a href="https://www.linkedin.com/in/rodrigo-camargo-a569b5179/">
+          <a href="https://www.linkedin.com/in/rodrigo-camargo-a569b5179/">
             <img style={{padding:10}}  align="left" alt="Rodrigo Camargo Linkedin" color="white" width="52pxpx" src="https://raw.githubusercontent.com/peterthehan/peterthehan/master/assets/linkedin.svg" />
           </a>   
+            <h6>Rodrigo</h6>
           <a href="https://www.linkedin.com/in/matheus-schneider-tallmann-973b081b4">
             <img style={{padding:10}} align="left" alt="Rodrigo Camargo Linkedin" color="white" width="52pxpx" src="https://raw.githubusercontent.com/peterthehan/peterthehan/master/assets/linkedin.svg" />
           </a>  
+            <h6>Matheus</h6>
           <a href="https://www.linkedin.com/in/marcos-vinicius-rocha-davi-alves-4b133072">
             <img style={{padding:10}} align="left" alt="Rodrigo Camargo Linkedin" color="white" width="52pxpx" src="https://raw.githubusercontent.com/peterthehan/peterthehan/master/assets/linkedin.svg" />
           </a>  
+            <h6>Marcos</h6>
           <a href="https://www.linkedin.com/in/vinicius-de-oliveira-2273821a0/">
             <img style={{padding:10}} align="left" alt="Rodrigo Camargo Linkedin" color="white" width="52pxpx" src="https://raw.githubusercontent.com/peterthehan/peterthehan/master/assets/linkedin.svg" />
           </a>      
+            <h6>Vinícius</h6>
            
           </div>
           
--- src/components/Footer/FooterStyles.js
+++ src/components/Footer/FooterStyles.js
@@ -1,10 +1,10 @@
 import styled from "styled-components";
-import { Nav } from "react-bootstrap";
 
 
 export const FooterStyled = styled.footer`
 color:#FFF;
 padding: 0;
 background-color: #282c34;
+display: inline;
 
 `
